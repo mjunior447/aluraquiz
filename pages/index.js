66dcac0763eed0c638011422f3b513e209cf04c5
Mauricio <mjunior447@gmail.com>
2021-01-28 18:33:46 -0300

    adicionada logo do alura-quiz; criada validação do nome e funcionalidade do botão Jogar; criadas as primeiras rotas para a página de quiz; criado protótipo da página de quiz;

--- pages/index.js
+++ pages/index.js
@@ -1,10 +1,14 @@
-import styled from 'styled-components'
-import db from '../db.json'
-import Widget from '../src/components/Widget'
-import GitHubCorner from '../src/components/GitHubCorner'
-import Footer from '../src/components/Footer'
-import QuizBackground from '../src/components/QuizBackground'
-import QuizLogo from '../src/components/QuizLogo'
+import React from 'react';
+import styled from 'styled-components';
+import Head from 'next/head';
+import { useRouter } from 'next/router';
+
+import db from '../db.json';
+import Widget from '../src/components/Widget';
+import GitHubCorner from '../src/components/GitHubCorner';
+import Footer from '../src/components/Footer';
+import QuizBackground from '../src/components/QuizBackground';
+import QuizLogo from '../src/components/QuizLogo';
 
 // const BackgroundImage = styled.div`
 //   background-image: url(${db.bg});
@@ -22,19 +26,43 @@ export const QuizContainer = styled.div`
     margin: auto;
     padding: 15px;
   }
-`
+`;
 
 export default function Home() {
+  const router = useRouter();
+  let [name, setName] = React.useState('');
+  console.log('retorno do usestate: ', name, setName);
+
   return (
-    <QuizBackground backgroundImage = {db.bg}>
+    <QuizBackground backgroundImage={db.bg}>
+      <Head>
+        <title>Alura - projeto base</title>
+      </Head>
       <QuizContainer>
-      <QuizLogo></QuizLogo>
+        <QuizLogo />
         <Widget>
           <Widget.Header>
             <h1>The legend of Zelda</h1>
           </Widget.Header>
           <Widget.Content>
-            <p>bla bla bla bla bla...</p>
+            <form onSubmit={function(evento) {
+              evento.preventDefault();
+              router.push(`/quiz?name=${name}`);
+              console.log('fazendo um envio de formulário via react');
+
+              // router manda para a próxima página
+            }}>
+              <input onChange={function(evento) {
+                console.log(evento.target.value);
+                // State
+                // name = evento.target.value;
+                setName(evento.target.value);
+              }} 
+              placeholder="fala teu nome aí, mané"/>
+              <button type="submit" disabled={ name.length === 0 }>
+                Jogar { name }
+              </button>
+            </form>
           </Widget.Content>
         </Widget>
         <Widget>
@@ -42,12 +70,12 @@ export default function Home() {
             <h1>Quiz da galera</h1>
           </Widget.Header>
           <Widget.Content>
-          <p>quizes do pessoal...</p>
+            <p>quizes do pessoal...</p>
           </Widget.Content>
         </Widget>
         <Footer />
       </QuizContainer>
-      <GitHubCorner projectUrl="https://github.com/mjunior447"/>
+      <GitHubCorner projectUrl="https://github.com/mjunior447" />
     </QuizBackground>
-  )
+  );
 }
---
+++ pages/quiz.js
@@ -0,0 +1,7 @@
+import React from 'react';
+
+export default function PudimComBatata() {
+    return (
+        <div>página de quiz</div>
+    );
+}
